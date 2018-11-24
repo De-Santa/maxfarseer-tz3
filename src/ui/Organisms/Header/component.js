c10e0a623d569b4cacbe3adc0532958e1562064e
German Topalov <ru00161131@gmail.com>
2018-11-24 22:28:11 +0700

    [update] Header styling updated

--- src/ui/Organisms/Header/component.js
+++ src/ui/Organisms/Header/component.js
@@ -1,20 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import T from 'prop-types'
+import { Button } from "../../Atoms/Button";
 import bemHelper from 'utils/bem-helper'
+import './styles.scss'
 
 const cn = bemHelper('header')
 
 export default class Header extends Component {
   static propTypes = {
+    authorized: T.bool.isRequired,
+    gApiLoading: T.bool.isRequired,
+    gApiLoaded: T.bool.isRequired,
+    gApiError: T.bool.isRequired,
     signIn: T.func.isRequired,
-    signOut: T.func.isRequired
+    signOut: T.func.isRequired,
+    userInfo: T.object,
+  }
+
+  static defaultProps = {
+    userInfo: null,
   }
 
   render() {
-    const { mix } = this.props
+    const { mix, authorized, gApiLoading, gApiLoaded, gApiError, signIn, signOut, userInfo } = this.props
+
     return (
       <header {...cn('', '', mix)}>
-        Header
+        <div {...cn('logo')}>Farseer <span>News</span></div>
+        <div {...cn('auth')}>
+          {authorized && (
+            <Fragment>
+              <img {...cn('user-avatar')} src={userInfo.avatar} alt={userInfo.firstName} />
+              <div {...cn('user-name')}>{userInfo.firstName}</div>
+            </Fragment>
+          )}
+          <Button
+            onClick={authorized ? signOut : signIn}
+            disabled={!gApiLoaded}
+          >
+            {gApiLoading && 'Инициализация Google API'}
+            {gApiError && 'Google API недоступен =('}
+            {gApiLoaded
+              ? authorized ? 'Выход из приложения' : 'Вход в приложение'
+              : null
+            }
+          </Button>
+        </div>
       </header>
     )
   }
--- src/ui/Organisms/Header/container.js
+++ src/ui/Organisms/Header/container.js
@@ -1,13 +1,31 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { initGoogleApi } from 'store/authorization';
+import { signIn, signOut } from 'store/authorization';
 import Component from './component';
+
+function mapStateToProps(state) {
+  const {
+    authorized,
+    gApiLoading,
+    gApiLoaded,
+    gApiError,
+    userInfo
+  } = state.authorization;
+
+  return {
+    authorized,
+    gApiLoading,
+    gApiLoaded,
+    gApiError,
+    userInfo
+  };
+}
 
 function mapDispatchToProps(dispatch) {
   const actions = bindActionCreators(
-    { initGoogleApi },
+    { signIn, signOut },
     dispatch);
   return { ...actions };
 }
 
-export const Header = connect(null, mapDispatchToProps)(Component);
+export const Header = connect(mapStateToProps, mapDispatchToProps)(Component);

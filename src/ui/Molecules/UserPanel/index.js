a40b54de491111aadcf6f0927a136e17899fce31
German Topalov <ru00161131@gmail.com>
2018-12-01 21:05:25 +0700

    [update] UserPanel molecule styles updated

--- src/ui/Molecules/UserPanel/index.js
+++ src/ui/Molecules/UserPanel/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import T from 'prop-types';
 import bemHelper from 'utils/bem-helper';
-import { Button } from "../../Atoms/Button";
 import './styles.scss';
 
 const cn = bemHelper('user-panel');
@@ -9,7 +8,6 @@ const cn = bemHelper('user-panel');
 export const UserPanel = ({ mix, userInfo }) => (
   <div {...cn(null, null, mix)}>
     <p {...cn('greeting')}>Привет {userInfo.fullName}!</p>
-    <Button mix={cn('add-btn').className}>Добавить новость</Button>
   </div>
 )
 

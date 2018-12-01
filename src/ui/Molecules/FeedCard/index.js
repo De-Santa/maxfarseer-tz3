6536123777a21dd76edd001e76233b9e76e3285c
German Topalov <ru00161131@gmail.com>
2018-12-01 21:06:02 +0700

    [update] FeedCard molecule updated

--- src/ui/Molecules/FeedCard/index.js
+++ src/ui/Molecules/FeedCard/index.js
@@ -5,29 +5,41 @@ import './styles.scss';
 
 const cn = bemHelper('feed-card');
 
-export const FeedCard = ({
-  mix, content, creator, title, createDate
-}) => (
-  <div {...cn(null, null, mix)}>
-    <div {...cn('top')}>
-      <div {...cn('info')}>
-        <h2 {...cn('title')}>{title}</h2>
-        <p {...cn('date')}>{createDate}</p>
-      </div>
-    </div>
-    <p {...cn('content')}>{content}</p>
-    <p {...cn('author')}>{creator.displayName}</p>
-  </div>
-)
-
-FeedCard.propTypes = {
+const _propTypes = {
   mix: T.string,
   content: T.string.isRequired,
   creator: T.object.isRequired,
   createDate: T.string.isRequired,
   title: T.string.isRequired
-};
+}
 
-FeedCard.defaulrProps = {
+const _defaultProps = {
   mix: ''
-};
+}
+
+export const FeedCard = ({
+  mix, content, creator, title, createDate, authorized, userInfo
+}) => {
+  const userIsCreator = authorized && creator._id === userInfo.id
+
+  return (
+    <div {...cn(null, null, mix)}>
+      <div {...cn('top')}>
+        {userIsCreator && (
+          <div {...cn('controls')}>
+            Вы автор!
+          </div>
+        )}
+        <div {...cn('info')}>
+          <h2 {...cn('title')}>{title}</h2>
+          <p {...cn('date')}>{createDate}</p>
+        </div>
+      </div>
+      <p {...cn('content')}>{content}</p>
+      <p {...cn('author')}>{creator.displayName}</p>
+    </div>
+  )
+}
+
+FeedCard.propTypes = _propTypes
+FeedCard.defaultProps = _defaultProps

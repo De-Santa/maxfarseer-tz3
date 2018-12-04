7d923946bb21fb0f72eed78d9e11ac7ba59f1a46
German Topalov <ru00161131@gmail.com>
2018-12-05 02:30:45 +0700

    [update] Loader ui atom added

---
+++ src/ui/Atoms/Loader/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as T from 'prop-types';
+import bemHelper from 'utils/bem-helper';
+import './styles.scss';
+
+const cn = bemHelper('loader');
+
+const Loader = ({ mix, size }) => (
+  <div {...cn('', '', mix)} style={{ width: size, height: size }}>
+    <svg {...cn('spinner')} viewBox="25 25 50 50">
+      <circle {...cn('path')} cx="50" cy="50" r="20" fill="none" strokeWidth="3" strokeMiterlimit="10" />
+    </svg>
+  </div>
+);
+
+Loader.propTypes = {
+  mix: T.string,
+  size: T.string,
+};
+
+Loader.defaultProps = {
+  mix: '',
+  size: '30px'
+};
+
+export default Loader;
